09e3e59fa36c4516b57c3ae18024efb950a51531
nefelin <nephilim.eric@gmail.com>
2021-05-12 14:19:39 -0400

    Added svgr plugin

---
+++ projects/spa/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  // testMatch: [
+  //   '../**/__tests__/**/*.[jt]s?(x)',
+  //   '../**/?(*.)+(spec|test).[tj]s?(x)',
+  //   'src/temp.test.ts'
+  // ],
+};
