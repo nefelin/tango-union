6a29cf3db7aa85acfa6a4dc7f105fc898b6230a3
nefelin <nephilim.eric@gmail.com>
2021-05-12 14:24:25 -0400

    re-organized config - jest

--- projects/spa/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  // testMatch: [
-  //   '../**/__tests__/**/*.[jt]s?(x)',
-  //   '../**/?(*.)+(spec|test).[tj]s?(x)',
-  //   'src/temp.test.ts'
-  // ],
-};
