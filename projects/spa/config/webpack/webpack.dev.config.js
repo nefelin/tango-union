a5c83b3726e5480957fb35345094878eba44dda6
nefelin <nephilim.eric@gmail.com>
2021-05-12 15:01:33 -0400

    re-organized config - tsc, babel, webpack

--- projects/spa/config/webpack/webpack.dev.config.js
+++ projects/spa/config/webpack/webpack.dev.config.js
@@ -2,26 +2,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
-
+const babelOptions = require('./babelrc');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, './dist'),
+    contentBase: path.resolve(__dirname, '../../dist'),
     open: true,
     compress: true,
     hot: true,
     port: 8080,
   },
   entry: {
-    main: path.resolve(__dirname, './src/index.tsx'),
+    main: path.resolve(__dirname, '../../src/index.tsx'),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ['babel-loader', 'ts-loader'],
+        use: [
+          { loader: 'babel-loader', options: babelOptions },
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(
+                __dirname,
+                '../typescript/tsconfig.dev.json',
+              ),
+            },
+          },
+        ],
         exclude: /node_modules/,
       },
       {
@@ -39,19 +50,19 @@ module.exports = {
       {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
-      }
+      },
     ],
   },
   resolve: {
     extensions: ['.wasm', '.ts', '.tsx', '.mjs', '.cjs', '.js', '.json'],
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../../dist'),
     filename: '[name].bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/template.html'),
+      template: path.resolve(__dirname, '../../src/template.html'),
     }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
