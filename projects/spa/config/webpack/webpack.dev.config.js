b396995eee59118b27b027e8997071b21090599a
nefelin <nephilim.eric@gmail.com>
2021-05-12 17:10:35 -0400

    webpack configs split and production build wired up. added bundle analyzers

--- projects/spa/config/webpack/webpack.dev.config.js
+++ projects/spa/config/webpack/webpack.dev.config.js
@@ -1,70 +1,18 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
-const babelOptions = require('./babelrc');
-module.exports = {
+const { merge } = require('webpack-merge');
+const sharedWebpackConfig = require('./webpack.shared.config');
+
+module.exports = merge(sharedWebpackConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     historyApiFallback: true,
     contentBase: path.resolve(__dirname, '../../dist'),
-    open: true,
+    open: false,
     compress: true,
     hot: true,
     port: 8080,
   },
-  entry: {
-    main: path.resolve(__dirname, '../../src/index.tsx'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          { loader: 'babel-loader', options: babelOptions },
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: path.resolve(
-                __dirname,
-                '../typescript/tsconfig.dev.json',
-              ),
-            },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|jpe?g|gif|jp2|webp)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/inline',
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.wasm', '.ts', '.tsx', '.mjs', '.cjs', '.js', '.json'],
-  },
-  output: {
-    path: path.resolve(__dirname, '../../dist'),
-    filename: '[name].bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../../src/template.html'),
-    }),
-    new CleanWebpackPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-};
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
---
+++ projects/spa/config/webpack/webpack.shared.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const babelOptions = require('./babelrc');
+const postcssOptions = require('./postcss.config');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '../../src/index.tsx'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          { loader: 'babel-loader', options: babelOptions },
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(
+                __dirname,
+                '../typescript/tsconfig.dev.json',
+              ),
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif|jp2|webp)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          { loader: 'postcss-loader', options: { postcssOptions } },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.wasm', '.ts', '.tsx', '.mjs', '.cjs', '.js', '.json'],
+  },
+  output: {
+    path: path.resolve(__dirname, '../../dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../../src/template.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
