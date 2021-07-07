60d277c738a791fa971353bacea400d6c2249217
nefelin <nephilim.eric@gmail.com>
2021-07-06 21:55:33 -0400

    attempting to serve ssl

--- projects/api/src/main.ts
+++ projects/api/src/main.ts
@@ -1,23 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as http from 'http';
+import * as https from 'https';
+import express from 'express';
+
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const compression = require('compression');
-const PORT = process.env.PORT;
+const HTTP_PORT = process.env.HTTP_PORT;
+const HTTPS_PORT = process.env.HTTPS_PORT;
+
+const CERT_FILE = process.env.SSL_CERT_FILE;
+const PRIV_KEY = process.env.SSL_PRIVATE_KEY;
+
+const httpsOptions = !!CERT_FILE
+  ? {
+      key: fs.readFileSync(PRIV_KEY),
+      cert: fs.readFileSync(CERT_FILE),
+    }
+  : null;
 
 async function bootstrap() {
   const schemaGenerationMode = process.env.GENERATE_ONLY;
   if (schemaGenerationMode) {
     console.info('Starting nest app to generate schema...');
     const app = await NestFactory.create(AppModule);
-    await app.listen(PORT);
+    await app.listen(HTTP_PORT);
     console.info('Schema generated, exiting...');
     process.exit(0);
   }
 
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.use(compression());
-  await app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+
+  http.createServer(server).listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}...`));
+  if (httpsOptions) {
+    https.createServer(httpsOptions, server).listen(HTTPS_PORT, () => console.log(`Listening (SSL) on port ${HTTP_PORT}...`));
+  }
 }
 
 bootstrap();
