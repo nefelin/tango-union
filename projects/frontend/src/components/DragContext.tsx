8fc20595675399fce2b89d32092795bcda4416b6
Eric Lindgren <nephilim.eric@gmail.com>
2021-09-04 22:07:06 -0400

    adds same list sorting

--- projects/frontend/src/components/DragContext.tsx
+++ projects/frontend/src/components/DragContext.tsx
@@ -7,10 +7,12 @@ import { playlistIdFromListId, reactiveSongLists } from '../hooks/state/useGloba
 import { selectedTracks } from '../hooks/state/usePlaylistsState/util';
 import { useSearchbarState } from '../hooks/state/useSearchbarState';
 import { reactiveActivePlaylistId } from '../hooks/state/useSelectionState';
+import { CompactTrack, ListId } from '../types/CompactTrack';
 import { CustomDragMode } from './DragContext/props';
 import DndContext from './DragNDrop/DnDContext';
 import { Counter } from './DragNDrop/Dragger/Counter/Counter';
 import { DragOverEvent, State } from './DragNDrop/store/types';
+import { moveMany } from './Playlist/PlaylistBody/util';
 
 const DragContext: React.FunctionComponent = ({ children }) => {
   const [dragging, setDragging] = useState(false);
@@ -20,9 +22,26 @@ const DragContext: React.FunctionComponent = ({ children }) => {
   const handleDragStart = () => {
     // setDragging(true);
   };
-  const handleDragEnd = ({overId}: State) => {
-    console.log('active', reactiveActivePlaylistId())
-    console.log('over', playlistIdFromListId(overId ?? ''))
+  const handleDragEnd = (state: State) => {
+    const {overId, overPosition} = state;
+    const lists = reactiveSongLists()
+    const sourcePlaylistId = reactiveActivePlaylistId()
+    const destPlaylistId = playlistIdFromListId(overId ?? '')
+    const sourceList = lists[sourcePlaylistId ?? '']
+    const destList = lists[destPlaylistId ?? ''];
+
+    if (!sourceList || !destList) {
+      return;
+    }
+
+    const { selection } = sourceList;
+
+    if (destPlaylistId && overId && sourcePlaylistId === destPlaylistId) {
+      const forward = overPosition?.[0] === 'bottom';
+      const {tracks} = destList;
+      const newTracks = moveMany(tracks, [...selection], overId, forward)
+      reactiveSongLists({...lists, [destList.id]: {...destList, tracks: newTracks}})
+    }
     // if (dragMode === 'search') {
     //   const activeList = reactiveActivePlaylistId();
     //   if (activeList) {
--- projects/frontend/src/components/Playlist/PlaylistBody/util.ts
+++ projects/frontend/src/components/Playlist/PlaylistBody/util.ts
@@ -8,19 +8,19 @@ export function moveMany(
   forward: boolean,
 ): Array<CompactTrack> {
   const cloneArray = ar.slice();
-  const orderedSources = cloneArray.filter((id) =>
-    sourceIds.some((sourceId) => sourceId === id[1]),
+  const orderedSources = cloneArray.filter(({listId}) =>
+    sourceIds.some((sourceId) => sourceId === listId),
   );
-  const insertAt = cloneArray.findIndex((id) => id[1] === targetId); // id accessors should be functions
+  const insertAt = cloneArray.findIndex(({listId}) => listId === targetId); // id accessors should be functions
   const adjustment = forward ? 1 : 0;
 
   return [
     cloneArray
       .slice(0, insertAt + adjustment)
-      .filter((id) => !sourceIds.includes(id[1])),
+      .filter(({listId}) => !sourceIds.includes(listId)),
     orderedSources,
     cloneArray
       .slice(insertAt + adjustment, cloneArray.length)
-      .filter((id) => !sourceIds.includes(id[1])),
+      .filter(({listId}) => !sourceIds.includes(listId)),
   ].reduce((prev, curr) => [...prev, ...curr]);
 }
