62033d2ddf56e1f834f42b3cba41e11d6f99f8dc
Eric Lindgren <nephilim.eric@gmail.com>
2021-09-17 16:43:45 -0400

    margins tweak

--- projects/frontend/src/components/TopBar.tsx
+++ projects/frontend/src/components/TopBar.tsx
@@ -32,7 +32,7 @@ const TopBarContainer = styled.div`
   width: 100%;
   display: flex;
   padding-left: 20px;
-  margin-bottom: 20px;
+  margin-bottom: 5px;
   background-color: #0606b9;
   align-items: center;
 `;
