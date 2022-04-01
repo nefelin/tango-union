7693bd13ffa5d7dd68f8585376c0d64ed7730a0d
Eric Lindgren <nephilim.eric@gmail.com>
2022-04-01 15:07:05 -0700

    adds CompactTrack type documentation

--- projects/frontend/src/types/compactTrack/types.ts
+++ projects/frontend/src/types/compactTrack/types.ts
@@ -11,6 +11,13 @@ type MaybeVideoId = `${Delimiter}${VideoId}` | '';
 
 export type CompoundIdString = `${ListId}${Delimiter}${TrackId}${MaybeVideoId}`;
 
+/** CompactTrack is meant to hold track info in contexts with the following priorities:
+ *  - string representational (i.e. url params)
+ *  - brevity (anywhere we don't want full track info)
+ *  - distinguishable ordered duplicates allowed
+ *  - optionally specific video link specified
+**/
+
 export interface CompactTrack {
   listId: ListId; // this is the only guaranteed unique id on this object, rename?
   trackId: TrackId;
