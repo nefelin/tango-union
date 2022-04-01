2954450e5113424e0bec7b73e8794a1c415b8a0e
Eric Lindgren <nephilim.eric@gmail.com>
2022-04-01 14:18:53 -0700

    fixes MobilePanelLayout.stories.tsx mock data

--- projects/frontend/src/stories/MobilePanelLayout.stories.tsx
+++ projects/frontend/src/stories/MobilePanelLayout.stories.tsx
@@ -1,23 +1,33 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 
-import AnimatedEq from '../components/AnimatedEq';
+import { MobileSearchProps } from '../components/MobileSearch';
+import { darienzoLaborde } from '../components/PlaylistSummary/summarize.test.data';
 import MobileDashBody from '../features/MobileDash/MobileDashBody';
+import { playlistTrackFromTrack } from '../types/compactTrack/util';
+import { compoundQuery } from './queries/compoundQuery';
 import MockProvider from './util/MockProvider';
 
 export default {
   title: 'Pages/Mobile/Panel Layout',
-  component: AnimatedEq,
+  component: MobileDashBody,
   parameters: {},
-} as ComponentMeta<typeof AnimatedEq>;
+} as ComponentMeta<typeof MobileDashBody>;
 
-const Template: ComponentStory<typeof AnimatedEq> = (args) => {
+const Template: ComponentStory<typeof MobileDashBody> = (args) => {
   return (
     <MockProvider>
-      {/*<MobileDashBody />*/}
+      <MobileDashBody {...args}/>
     </MockProvider>
   );
 };
 
-export const Basic = Template.bind({});
-Basic.args = {};
+export const Default = Template.bind({});
+Default.args = {
+  compoundQuery: compoundQuery,
+  resetSearch: () => {},
+  setSearch: () => {},
+  initSearchState: {},
+  playlistTracks: darienzoLaborde.map(playlistTrackFromTrack),
+  resultsTracks: darienzoLaborde.map(playlistTrackFromTrack),
+};
