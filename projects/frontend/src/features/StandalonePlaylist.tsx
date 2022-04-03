b9a0b93d54c66363f9b9616abf25d99231a8a5fd
Eric Lindgren <nephilim.eric@gmail.com>
2022-04-02 17:58:18 -0700

    fixes StandalonePlaylist.tsx layout issues

--- projects/frontend/src/features/StandalonePlaylist.tsx
+++ projects/frontend/src/features/StandalonePlaylist.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 import ResponsivePlaylistContainer from '../components/ResponsivePlaylist/ResponsivePlaylistContainer';
 import StandaloneFraming from '../components/ResponsivePlaylist/StandaloneFraming';
 import TopBar from '../components/TopBar';
+import { useIsMobile } from '../util/isMobile';
+import { asVh, layout } from './MobileDash/layout';
 
 const StandalonePlaylist = () => {
-  return <><TopBar/><StandaloneFraming><ResponsivePlaylistContainer/></StandaloneFraming></>
+  const isMobile = useIsMobile();
+  const mobileLayoutAdjustments: CSSProperties = isMobile
+    ? { paddingTop: asVh(layout.topbar), boxSizing: 'border-box' }
+    : {};
+  return (
+    <>
+      <TopBar fixed={isMobile} />
+      <div style={{...mobileLayoutAdjustments}}>
+        <StandaloneFraming>
+          <ResponsivePlaylistContainer />
+        </StandaloneFraming>
+      </div>
+    </>
+  );
 };
 
 export default StandalonePlaylist;
---
+++ projects/frontend/src/util/isMobile.ts
@@ -0,0 +1,3 @@
+import { useMediaQuery } from 'react-responsive';
+
+export const useIsMobile = () => useMediaQuery({ query: '(max-width: 1024px)' });
