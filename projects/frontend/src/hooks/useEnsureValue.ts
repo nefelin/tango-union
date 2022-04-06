993d38f1110717558159d86aea5aa3075d86c99a
Eric Lindgren <nephilim.eric@gmail.com>
2022-04-05 22:43:46 -0700

    adds a non-reactive version of useEnsureValue.ts

--- projects/frontend/src/hooks/useEnsureValue.ts
+++ projects/frontend/src/hooks/useEnsureValue.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function useEnsureValue<Value, Fallback>(value: Value, fallback: Fallback ) : NonNullable<Value> | Fallback{
   const [retained, setRetained] = useState<Value | Fallback>(fallback);
@@ -11,6 +11,20 @@ function useEnsureValue<Value, Fallback>(value: Value, fallback: Fallback ) : No
 
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
   return retained as NonNullable<Value> | Fallback;
+}
+
+
+export function useEnsureValueNonReactive<Value, Fallback>(value: Value, fallback: Fallback ) : NonNullable<Value> | Fallback{
+  const retained = useRef<Value | Fallback>(fallback);
+
+  useEffect(() => {
+    if (value !== undefined && value !== null) {
+      retained.current = value
+    }
+  }, [value])
+
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  return retained.current as NonNullable<Value> | Fallback;
 }
 
 export default useEnsureValue;
