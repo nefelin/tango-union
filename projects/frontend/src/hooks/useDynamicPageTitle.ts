bc6ff2694e91fb546170afa180b2b4f0f42284fc
Eric Lindgren <nephilim.eric@gmail.com>
2022-04-05 22:40:25 -0700

    adds a non-reactive version of useEnsureValue.ts

---
+++ projects/frontend/src/hooks/useDynamicPageTitle.ts
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+
+import prioritizeSummary from '../components/PlaylistSummary/prioritizeSummary';
+import { smartSummary } from '../components/PlaylistSummary/summarize';
+import useCacheStitchedIdFetch from '../components/ResultsTable/useCacheStitchedIdFetch';
+import { CompactTrack } from '../types/compactTrack/types';
+import {
+  QUICKLIST_PLAYLIST_ID,
+  RESULTS_PLAYLIST_ID,
+} from './state/useGlobalPlaylistState/songLists.state';
+import { usePlaylistState } from './state/usePlaylistState';
+import useEnsureValue, { useEnsureValueNonReactive } from './useEnsureValue';
+
+const DEFAULT_TITLE = 'Tango Union - Explore Argentine Tango Music';
+
+const useDynamicPageTitle = () => {
+  const {
+    playlist: { tracks: playlistTracks },
+  } = usePlaylistState(QUICKLIST_PLAYLIST_ID);
+  const {
+    playlist: { tracks: resultsTracks },
+  } = usePlaylistState(RESULTS_PLAYLIST_ID);
+
+  let dominantList: Array<CompactTrack> = [];
+  if (playlistTracks.length) {
+    dominantList = playlistTracks;
+  } else {
+    dominantList = resultsTracks;
+  }
+
+  const [hydratedTracks] = useCacheStitchedIdFetch(dominantList);
+
+  const ensuredTracks = useEnsureValueNonReactive(hydratedTracks, []);
+
+  console.log({ensuredTracks})
+  const [headline, subtitle] = prioritizeSummary(smartSummary(hydratedTracks || []));
+
+  useEffect(() => {
+    const newTitle = headline ? `${headline} - ${subtitle}` : DEFAULT_TITLE;
+    document.title = newTitle;
+  }, [headline, subtitle]);
+};
+
+export default useDynamicPageTitle;
