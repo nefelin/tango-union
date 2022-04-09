c367d37cf1b750958b6b37e99e6de8e5da340d22
Eric Lindgren <nephilim.eric@gmail.com>
2022-04-09 15:55:09 -0700

    fixes FF button after change in results

--- projects/frontend/src/hooks/state/useGlobalPlaylistState.ts
+++ projects/frontend/src/hooks/state/useGlobalPlaylistState.ts
@@ -2,8 +2,12 @@ import { useReactiveVar } from '@apollo/client';
 import { useEffect, useState } from 'react';
 
 import { PlayingContext } from './useGlobalPlaylistState/PlayingContext.type';
-import { reactiveSongLists } from './useGlobalPlaylistState/songLists.state';
+import {
+  reactiveSongLists,
+  RESULTS_PLAYLIST_ID,
+} from './useGlobalPlaylistState/songLists.state';
 import { createContext } from './useGlobalPlaylistState/util';
+import { usePlaylistState } from './usePlaylistState';
 import { useYoutubePlayerState } from './useYoutubePlayerState';
 
 const initContext: PlayingContext = {};
@@ -12,6 +16,7 @@ export const useGlobalPlaylistsState = () => {
   const lists = useReactiveVar(reactiveSongLists);
   const { currentTrack } = useYoutubePlayerState();
   const [context, setContext] = useState<PlayingContext>(initContext);
+  const { playlist: resultsList } = usePlaylistState(RESULTS_PLAYLIST_ID);
 
   useEffect(() => {
     if (!currentTrack) {
@@ -19,17 +24,27 @@ export const useGlobalPlaylistsState = () => {
       return;
     }
 
+    let trackExistsInList = false;
     Object.values(lists).some((playlist) => {
       const index = playlist.tracks.findIndex(
-        ({listId}) => currentTrack.listId === listId
+        ({ listId }) => currentTrack.listId === listId,
       );
       if (index !== -1) {
         setContext(createContext(playlist, index));
+        trackExistsInList = true;
         return true;
       }
       return false;
     });
-  }, [currentTrack]);
+
+    if (!trackExistsInList) {
+      const fallbackContext: PlayingContext = {
+        ...createContext(resultsList, -1),
+        currentTrack,
+      };
+      setContext(fallbackContext);
+    }
+  }, [currentTrack, resultsList]);
 
   return {
     context,
